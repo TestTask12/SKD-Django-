d3e296b155ef0f24dcc46a5c9687678b6ac8e93a
suman <sumankumariideal@gmail.com>
2021-07-21 18:10:24 +0530

    “initial”

---
+++ README.md
@@ -0,0 +1,2 @@
+# SKD-Django-
+Django code
---
+++ backend1/prodapi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProdapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'prodapi'
